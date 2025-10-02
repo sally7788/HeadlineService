1905457e72ae0febfd347054ac20012b7b009150
sally7788 <0702sally@gmail.com>
2025-10-02 10:37:03 +0900

    view 수정1

--- news_project/news/urls.py
+++ news_project/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
 
-app_name = 'polls'
+app_name = 'news_project'
 urlpatterns = [
     path('news/', NewsList.as_view(), name='newslist'),
     path('publisher/', PublisherList.as_view(), name='publisherlist'),
-    path('delete_publisher/<int:pk>', DeletePublisher.as_view(), name='deletepublisher')
+    path('delete_publisher/<int:pk>', DeletePublisher.as_view(), name='deletepublisher'),
+    path('wordcloud/', get_wordcloud_data, name='wordclouddata')
 ]
--- news_project/news/views.py
+++ news_project/news/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .serializers import *
 from rest_framework import generics
 from rest_framework.response import Response
+from django.http import JsonResponse
+from django.db.models import Count
+from .models import *
+from .data_processing.wordcloud_generator import generate_word_frequencies
 
 # Create your views here.
 
@@ -16,3 +20,62 @@ class NewsList(generics.ListCreateAPIView):
 class DeletePublisher(generics.RetrieveDestroyAPIView):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
+
+def get_wordcloud_data(request):
+    
+    #사용자 요청 파라미터 가져오기 
+    start_date_req = request.GET.get('start_date')
+    end_date_req = request.GET.get('end_date')
+    
+    #publisher_ids를 어떻게 받아오지? 복수니까 리스트 찾아보기 
+    publishers_param= request.GET.get('publishers', '') 
+    publisher_ids_req = []
+
+    if publishers_param:
+        # URL에 publishers 값이 있으면 해당 ID만 사용
+        try:
+            publisher_ids_req = [int(p_id) for p_id in publishers_param.split(',')]
+        except ValueError:
+            return JsonResponse({'status': 'error', 'message': '유효하지 않은 발행처 ID입니다.'}, status=400)
+    else:
+        # URL에 publishers 값이 없으면 DB에 있는 모든 발행처 ID를 가져옴
+        publisher_ids_req = list(Publisher.objects.values_list('id', flat=True))
+        
+    #캐싱된 결과 조회 
+    latest_cached=None
+    
+    cached_results=WordCloudResult.objects.filter(
+        start_date=start_date_req,
+        end_date=end_date_req,
+    ).annotate(p_count=Count('included_publishers') #언론사 총 개수 
+    ).filter(p_count=len(publisher_ids_req)) # 연결된 언론사 수 = 요청된 발행사 수 
+    
+    #언론사 id 모두 포함하는지 
+    for pub_id in publisher_ids_req:
+        cached_results=cached_results.filter(included_publishers__id=pub_id)
+    latest_cached=cached_results.order_by('-created_at').first()
+    
+    #캐싱된 결과가 있으면 해당 결과 반환
+    if latest_cached:
+        print(">> 캐싱된 워드클라우드 결과를 반환합니다.")
+        return JsonResponse({
+            'status': 'success',
+            'result_id': latest_cached.id,
+            'data': latest_cached.word_frequency_json
+        })
+    else:
+        print(">> 캐싱된 결과가 없습니다. 새로운 워드클라우드 생성 작업을 시작합니다.")
+        
+        result=generate_word_frequencies(
+            start_date=start_date_req,
+            end_date=end_date_req,
+            selected_publisher_ids=publisher_ids_req
+        )
+        if result is None:
+            return JsonResponse({'status': 'error', 'message': '워드클라우드 생성에 실패했습니다. 조건에 맞는 기사가 없습니다.'}, status=500)
+        else:
+            return JsonResponse({
+                'status': 'success',
+                'result_id': result.id,
+                'data': result.word_frequency_json
+            })
