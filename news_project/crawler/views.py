1a83a6e91e13539786dec37300f5dfb894d54112
JM KIM <kjm9102@gmail.com>
2025-10-07 15:52:05 +0900

    swagger 연동

--- news_project/crawler/views.py
+++ news_project/crawler/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from . import crawler_youtube
+from . import crawler_naver
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def youtube_crawling(request):
@@ -10,3 +12,8 @@ def youtube_crawling(request):
         return HttpResponse("YouTube crawling completed successfully!")
     except Exception as e:
         return HttpResponse(f"Error: {str(e)}", status=500)
+    
+@api_view(['GET'])  
+def naver_crawling(request):
+    result = crawler_naver.crawl()  
+    return Response({'message': 'Naver crawling success', 'data': result})
